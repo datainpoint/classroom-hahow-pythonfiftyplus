670c3facbc1cd42235a48976bd156eb497b39477
yaojenkuo <tonykuoyj@gmail.com>
2022-01-02 15:26:52 +0800

    update

--- 07-modules/test_runner.py
+++ 07-modules/test_runner.py
@@ -1,79 +1,49 @@
 import unittest
 import json
 import ipynb.fs.full.exercises as ex
+from palindromefunctions import reverse_str
+from palindromefunctions import is_palindrome
+from palindromeclass import Palindrome
+from mymodules.palindromefunctions import reverse_str as mm_reverse_str
+from mymodules.palindromefunctions import is_palindrome as mm_is_palindrome
+from mymodules.palindromeclass import Palindrome as MmPalindrome
 
-class TestClasses(unittest.TestCase):
-    def test_046_reverse_str(self):
-        self.assertEqual(ex.reverse_str("eye"), 'eye')
-        self.assertEqual(ex.reverse_str("dad"), 'dad')
-        self.assertEqual(ex.reverse_str("dye"), 'eyd')
-        self.assertEqual(ex.reverse_str("mad"), 'dam')
-    def test_047_is_palindrome(self):
-        self.assertTrue(ex.is_palindrome("eye"))
-        self.assertTrue(ex.is_palindrome("dad"))
-        self.assertFalse(ex.is_palindrome("dye"))
-        self.assertFalse(ex.is_palindrome("mad"))
-    def test_048_PalindromeMethods(self):
-        palindrome_methods = ex.PalindromeMethods()
-        self.assertEqual(palindrome_methods.reverse_str("eye"), 'eye')
-        self.assertEqual(palindrome_methods.reverse_str("dad"), 'dad')
-        self.assertEqual(palindrome_methods.reverse_str("dye"), 'eyd')
-        self.assertEqual(palindrome_methods.reverse_str("mad"), 'dam')
-        self.assertTrue(palindrome_methods.is_palindrome("eye"))
-        self.assertTrue(palindrome_methods.is_palindrome("dad"))
-        self.assertFalse(palindrome_methods.is_palindrome("dye"))
-        self.assertFalse(palindrome_methods.is_palindrome("mad"))
-    def test_049_Palindrome(self):
-        palindrome = ex.Palindrome("eye")
+class TestModules(unittest.TestCase):
+    def test_046_functions_in_module(self):
+        self.assertEqual(reverse_str("eye"), 'eye')
+        self.assertEqual(reverse_str("dad"), 'dad')
+        self.assertEqual(reverse_str("dye"), 'eyd')
+        self.assertEqual(reverse_str("mad"), 'dam')
+        self.assertTrue(is_palindrome("eye"))
+        self.assertTrue(is_palindrome("dad"))
+        self.assertFalse(is_palindrome("dye"))
+        self.assertFalse(is_palindrome("mad"))
+    def test_047_classes_in_module(self):
+        palindrome = Palindrome("eye")
         self.assertEqual(palindrome.original_str, 'eye')
         self.assertEqual(palindrome.reversed_str, 'eye')
         self.assertTrue(palindrome.is_palindrome())
-        palindrome = ex.Palindrome("dye")
+        palindrome = Palindrome("dye")
         self.assertEqual(palindrome.original_str, 'dye')
         self.assertEqual(palindrome.reversed_str, 'eyd')
         self.assertFalse(palindrome.is_palindrome())
-    def test_050_collect_divisors_as_set(self):
-        self.assertEqual(ex.collect_divisors_as_set(3), {1, 3})
-        self.assertEqual(ex.collect_divisors_as_set(6), {1, 2, 3, 6})
-        self.assertEqual(ex.collect_divisors_as_set(4), {1, 2, 4})
-        self.assertEqual(ex.collect_divisors_as_set(8), {1, 2, 4, 8})
-    def test_051_find_common_divisors(self):
-        self.assertEqual(ex.find_common_divisors(3, 6), {1, 3})
-        self.assertEqual(ex.find_common_divisors(4, 8), {1, 2, 4})
-    def test_052_find_the_max_common_divisor(self):
-        self.assertEqual(ex.find_the_max_common_divisor(3, 6), 3)
-        self.assertEqual(ex.find_the_max_common_divisor(4, 8), 4)
-        self.assertEqual(ex.find_the_max_common_divisor(6, 8), 2)
-    def test_053_CommonDivisorMethods(self):
-        common_divisor_methods = ex.CommonDivisorMethods()
-        self.assertEqual(common_divisor_methods.collect_divisors_as_set(3), {1, 3})
-        self.assertEqual(common_divisor_methods.collect_divisors_as_set(6), {1, 2, 3, 6})
-        self.assertEqual(common_divisor_methods.find_common_divisors(3, 6), {1, 3})
-        self.assertEqual(common_divisor_methods.find_the_max_common_divisor(6, 8), 2)
-    def test_054_CommonDivisors(self):
-        common_divisors = ex.CommonDivisors(3, 6)
-        self.assertEqual(common_divisors.x_divisors, {1, 3})
-        self.assertEqual(common_divisors.y_divisors, {1, 2, 3, 6})
-        self.assertEqual(common_divisors.find_common_divisors(), {1, 3})
-        self.assertEqual(common_divisors.find_the_max_common_divisor(), 3)
-        common_divisors = ex.CommonDivisors(4, 8)
-        self.assertEqual(common_divisors.x_divisors, {1, 2, 4})
-        self.assertEqual(common_divisors.y_divisors, {1, 2, 4, 8})
-        self.assertEqual(common_divisors.find_common_divisors(), {1, 2, 4})
-        self.assertEqual(common_divisors.find_the_max_common_divisor(), 4)
-    def test_055_PrimeJudger(self):
-        prime_judger = ex.PrimeJudger(1)
-        self.assertEqual(prime_judger.x, 1)
-        self.assertEqual(prime_judger.count_number_of_divisors(), 1)
-        self.assertFalse(prime_judger.is_prime())
-        prime_judger = ex.PrimeJudger(2)
-        self.assertEqual(prime_judger.x, 2)
-        self.assertEqual(prime_judger.count_number_of_divisors(), 2)
-        self.assertTrue(prime_judger.is_prime())
-        prime_judger = ex.PrimeJudger(4)
-        self.assertEqual(prime_judger.x, 4)
-        self.assertEqual(prime_judger.count_number_of_divisors(), 3)
-        self.assertFalse(prime_judger.is_prime())
+    def test_048_folder_as_module(self):
+        self.assertEqual(mm_reverse_str("eye"), 'eye')
+        self.assertEqual(mm_reverse_str("dad"), 'dad')
+        self.assertEqual(mm_reverse_str("dye"), 'eyd')
+        self.assertEqual(mm_reverse_str("mad"), 'dam')
+        self.assertTrue(mm_is_palindrome("eye"))
+        self.assertTrue(mm_is_palindrome("dad"))
+        self.assertFalse(mm_is_palindrome("dye"))
+        self.assertFalse(mm_is_palindrome("mad"))
+        palindrome = MmPalindrome("eye")
+        self.assertEqual(palindrome.original_str, 'eye')
+        self.assertEqual(palindrome.reversed_str, 'eye')
+        self.assertTrue(palindrome.is_palindrome())
+        palindrome = MmPalindrome("dye")
+        self.assertEqual(palindrome.original_str, 'dye')
+        self.assertEqual(palindrome.reversed_str, 'eyd')
+        self.assertFalse(palindrome.is_palindrome())
 
 def run_suite(test_class, chapter_index):
     suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
@@ -110,4 +80,4 @@ def run_suite(test_class, chapter_index):
     elif chapter_percentage >= 50:
         print("你已經完成「{}」章節一半以上的練習，繼續加油！".format(chapter_name))
     
-run_suite(TestClasses, 5)
+run_suite(TestModules, 6)
