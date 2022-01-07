2e22783584bcc8fedcba7221992cdf4f82ea8496
yaojenkuo <tonykuoyj@gmail.com>
2022-01-07 23:10:47 +0800

    update

--- 11-pandas/test_runner.py
+++ 11-pandas/test_runner.py
@@ -19,27 +19,27 @@ class TestPandas(unittest.TestCase):
         first_five_evens_series = ex.create_first_five_evens_series()
         self.assertEqual(first_five_evens_series.size, 5)
         self.assertEqual(first_five_evens_series[0], 0)
-        self.assertEqual(first_five_evens_series[-1], 8)
+        self.assertEqual(first_five_evens_series[4], 8)
         self.assertIsInstance(first_five_evens_series, pd.core.series.Series)
     def test_84_update_first_five_evens_series_index(self):
         updated_first_five_evens_series_index = ex.update_first_five_evens_series_index()
         self.assertIsInstance(updated_first_five_evens_series_index, pd.core.series.Series)
-        self.assertEqual(updated_first_five_evens_series_index.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
-        self.assertEqual(updated_first_five_evens_series_index.values, np.array([0, 2, 4, 6, 8]))
+        pd.testing.assert_index_equal(updated_first_five_evens_series_index.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
+        np.testing.assert_array_equal(updated_first_five_evens_series_index.values, np.array([0, 2, 4, 6, 8]))
     def test_85_create_first_five_odds_series(self):
         first_five_odds_series = ex.create_first_five_odds_series()
         self.assertIsInstance(first_five_odds_series, pd.core.series.Series)
-        self.assertEqual(first_five_odds_series.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
-        self.assertEqual(first_five_odds_series.values, np.array([1, 3, 5, 7, 9]))
+        pd.testing.assert_index_equal(first_five_odds_series.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
+        np.testing.assert_array_equal(first_five_odds_series.values, np.array([1, 3, 5, 7, 9]))
     def test_86_create_first_five_primes_series(self):
         first_five_primes_series = ex.create_first_five_primes_series()
         self.assertIsInstance(first_five_primes_series, pd.core.series.Series)
-        self.assertEqual(first_five_primes_series.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
-        self.assertEqual(first_five_primes_series.values, np.array([2, 3, 5, 7, 11]))
+        pd.testing.assert_index_equal(first_five_primes_series.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
+        np.testing.assert_array_equal(first_five_primes_series.values, np.array([2, 3, 5, 7, 11]))
     def test_87_create_first_five_integer_dataframe(self):
         first_five_integer_dataframe = ex.create_first_five_integer_dataframe()
         self.assertIsInstance(first_five_integer_dataframe, pd.core.frame.DataFrame)
-        self.assertEqual(first_five_integer_dataframe.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
+        pd.testing.assert_index_equal(first_five_integer_dataframe.index, pd.Index(['1st', '2nd', '3rd', '4th', '5th']))
         self.assertEqual(first_five_integer_dataframe.shape, (5, 3))
     def test_88_create_lord_of_the_rings_dataframe(self):
         lord_of_the_rings_dataframe = ex.create_lord_of_the_rings_dataframe()
@@ -54,10 +54,10 @@ class TestPandas(unittest.TestCase):
         dark_knight_dataframe = ex.create_dark_knight_dataframe()
         summary = ex.summarize_dataframe_with_dict(lord_of_the_rings_dataframe)
         self.assertEqual(summary["shape"], (3, 4))
-        self.assertEqual(summary["columns"], pd.Index(['title', 'director', 'imdb_rating', 'release_year']))
+        pd.testing.assert_index_equal(summary["columns"], pd.Index(['title', 'director', 'imdb_rating', 'release_year']))
         summary = ex.summarize_dataframe_with_dict(dark_knight_dataframe)
         self.assertEqual(summary["shape"], (3, 4))
-        self.assertEqual(summary["columns"], pd.Index(['title', 'director', 'imdb_rating', 'release_year']))
+        pd.testing.assert_index_equal(summary["columns"], pd.Index(['title', 'director', 'imdb_rating', 'release_year']))
 
 def run_suite(test_class, chapter_index):
     suite = unittest.TestLoader().loadTestsFromTestCase(test_class)
