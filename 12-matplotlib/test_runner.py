db5202141e4aa328f95d21367d81024f11aca926
yaojenkuo <tonykuoyj@gmail.com>
2022-01-10 19:45:37 +0800

    delete

--- 12-matplotlib/test_runner.py
+++ 12-matplotlib/test_runner.py
@@ -7,7 +7,7 @@ import matplotlib as mpl
 from PIL import Image
 
 class TestMatplotlib(unittest.TestCase):
-    def test_91_create_x_cosx_ndarray(self)
+    def test_91_create_x_cosx_ndarray(self):
         x, cosx = ex.create_x_cosx_ndarray()
         self.assertIsInstance(x, np.ndarray)
         self.assertIsInstance(cosx, np.ndarray)
@@ -21,14 +21,14 @@ class TestMatplotlib(unittest.TestCase):
         ex.export_x_cosx_png()
         im = Image.open("xcosx.png")
         self.assertEqual(im.format, 'PNG')
-        im_test = Image.open("xcosx_test.png")
+        im_test = Image.open("/home/jovyan/test_images/xcosx.png")
         diff_sum = (np.array(im) - np.array(im_test)).sum()
         self.assertEqual(diff_sum, 0)
     def test_94_export_x_cosx_jpg(self):
         ex.export_x_cosx_jpg()
         im = Image.open("xcosx.jpg")
         self.assertEqual(im.format, 'JPEG')
-        im_test = Image.open("xcosx_test.jpg")
+        im_test = Image.open("/home/jovyan/test_images/xcosx.jpg")
         diff_sum = (np.array(im) - np.array(im_test)).sum()
         self.assertEqual(diff_sum, 0)
     def test_95_create_x_tanx_ndarray(self):
@@ -49,28 +49,28 @@ class TestMatplotlib(unittest.TestCase):
         ex.export_x_tanx_png()
         im = Image.open("xtanx.png")
         self.assertEqual(im.format, 'PNG')
-        im_test = Image.open("xtanx_test.png")
+        im_test = Image.open("/home/jovyan/test_images/xtanx.png")
         diff_sum = (np.array(im) - np.array(im_test)).sum()
         self.assertEqual(diff_sum, 0)
     def test_98_export_x_tanx_jpg(self):
         ex.export_x_tanx_jpg()
         im = Image.open("xtanx.jpg")
         self.assertEqual(im.format, 'JPEG')
-        im_test = Image.open("xtanx_test.jpg")
+        im_test = Image.open("/home/jovyan/test_images/xtanx.jpg")
         diff_sum = (np.array(im) - np.array(im_test)).sum()
         self.assertEqual(diff_sum, 0)
     def test_99_export_x_tanx_png_with_title(self):
         ex.export_x_tanx_png_with_title()
         im = Image.open("xtanx_with_title.png")
         self.assertEqual(im.format, 'PNG')
-        im_test = Image.open("xtanx_with_title_test.png")
+        im_test = Image.open("/home/jovyan/test_images/xtanx_with_title.png")
         diff_sum = (np.array(im) - np.array(im_test)).sum()
         self.assertEqual(diff_sum, 0)
     def test_100_export_x_tanx_png_with_title(self):
         ex.export_x_tanx_png_with_title()
         im = Image.open("xtanx_final.png")
         self.assertEqual(im.format, 'PNG')
-        im_test = Image.open("xtanx_final_test.png")
+        im_test = Image.open("/home/jovyan/test_images/xtanx_final.png")
         diff_sum = (np.array(im) - np.array(im_test)).sum()
         self.assertEqual(diff_sum, 0)
 
